4790b1e444dcd54df285a7b0951f44c7187bec7c
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-16 14:55:44 -0300

    64 commit

--- frontend/services/BookServices.js
+++ frontend/services/BookServices.js
@@ -4,7 +4,7 @@ class BookService {
     }
 
     async getBooks() {
-        const response = await fetch(this.URI);
+        const response = fetch(this.URI);
         const books = await response.json();
         return books
     }
