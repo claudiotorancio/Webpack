69b9dabd85f126fdba6c689fcddec6a88a6d05bb
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-21 23:59:18 -0300

    151 commit

--- frontend/UI.js
+++ frontend/UI.js
@@ -1,5 +1,5 @@
 
-import BookService from '../api/BookServices.js';
+import BookService from './services/BookServices.js';
 const bookService = new BookService();
 import { format } from 'timeago.js';
 
