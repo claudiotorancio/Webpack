e2d52c73f6a9c10dd11b7a97affb3f1f4b65a658
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-23 19:45:48 -0300

    176 commit

--- frontend/services/BookServices.js
+++ frontend/services/BookServices.js
@@ -1,36 +1,57 @@
-
-
-
 class BookService {
     constructor() {
-        this.URI = '/api/books'
+        this.URI = '/api/books';
     }
 
     async getBooks() {
-        const response = await fetch(this.URI);
-        console.log(response)
-        const books = await response.json();
-        return books
+        try {
+            const response = await fetch(this.URI);
+            if (!response.ok) {
+                throw new Error('Error al obtener libros');
+            }
+            const books = await response.json();
+            return books;
+        } catch (error) {
+            throw new Error('Error al obtener libros: ' + error.message);
+        }
     }
+
     async postBook(book) {
-        const res = await fetch(this.URI, {
-            method: 'POST',
-            body: book
-        });
-        const data = await res.json();
-        console.log(data)
+        try {
+            const res = await fetch(this.URI, {
+                method: 'POST',
+                body: JSON.stringify(book),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (!res.ok) {
+                throw new Error('Error al guardar el libro');
+            }
+            const data = await res.json();
+            return data;
+        } catch (error) {
+            throw new Error('Error al guardar el libro: ' + error.message);
+        }
     }
+
     async deleteBook(bookId) {
-        const res = await fetch(`${this.URI}/${bookId}`, {
-            headers: {
-                'Content-type': 'application/json'
-            },
-            method: 'DELETE',
-        });
-        const data = await res.json();
-        console.log(data);
+        try {
+            const res = await fetch(`${this.URI}/${bookId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (!res.ok) {
+                throw new Error('Error al eliminar el libro');
+            }
+            const data = await res.json();
+            return data;
+        } catch (error) {
+            throw new Error('Error al eliminar el libro: ' + error.message);
+        }
     }
 }
 
 export default BookService;
-
