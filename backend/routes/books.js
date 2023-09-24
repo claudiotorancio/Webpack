6fc7000837b05f9d629260378a2df17c0f39683c
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-24 20:10:25 -0300

    187 commit

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,31 +1,45 @@
-//routes/books.js
-
-const {Router} = require('express')
+const { Router } = require('express');
 const router = Router();
-const {unlink} = require('fs-extra')
-const path = require('path')
+const { unlink } = require('fs-extra');
+const path = require('path');
 
-const Book = require('../models/Book.js')
+const Book = require('../models/Book.js');
 
+// Ruta para obtener todos los libros (GET /api/books)
 router.get('/', async (req, res) => {
-    const books = await Book.find()
-    res.json(books)
+    try {
+        const books = await Book.find();
+        res.json(books);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 });
 
+// Ruta para agregar un nuevo libro (POST /api/books)
 router.post('/', async (req, res) => {
-    const{title, author, isbn } = req.body;
-    const imagePath = '/uploads/' + req.file.filename
-    const newBook = new Book ({title, author, isbn, imagePath});
-    await newBook.save();
+    try {
+        const { title, author, isbn, imagePath } = req.body;
+        const newBook = new Book({ title, author, isbn, imagePath });
+        await newBook.save();
 
-    res.json({message:'Book saved'})
+        res.json({ message: 'Book saved' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 });
 
+// Ruta para eliminar un libro por su ID (DELETE /api/books/:id)
 router.delete('/:id', async (req, res) => {
-    const book = await Book.findByIdAndDelete(req.params.id)
-    unlink(path.resolve('./backend/public' + book.imagePath))
-    res.json({message:'Book deleted'})
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id);
+        unlink(path.resolve('./backend/public' + book.imagePath));
+        res.json({ message: 'Book deleted' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 });
-
 
 module.exports = router;
