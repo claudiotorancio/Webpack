e93d0eb914e599d9b3954e33111d146e7b80d46b
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-25 00:04:54 -0300

    196 commit

--- api/books.js
+++ api/books.js
@@ -1,45 +1,31 @@
-const { Router } = require('express');
+//routes/books.js
+
+const {Router} = require('express')
 const router = Router();
-const { unlink } = require('fs-extra');
-const path = require('path');
+const {unlink} = require('fs-extra')
+const path = require('path')
 
-const Book = require('../backend/models/Book.js');
+const Book = require('./Book.js')
 
-// Ruta para obtener todos los libros (GET /api/books)
 router.get('/', async (req, res) => {
-    try {
-        const books = await Book.find();
-        res.json(books);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+    const books = await Book.find()
+    res.json(books)
 });
 
-// Ruta para agregar un nuevo libro (POST /api/books)
 router.post('/', async (req, res) => {
-    try {
-        const { title, author, isbn, imagePath } = req.body;
-        const newBook = new Book({ title, author, isbn, imagePath });
-        await newBook.save();
+    const{title, author, isbn } = req.body;
+    const imagePath = '/uploads/' + req.file.filename
+    const newBook = new Book ({title, author, isbn, imagePath});
+    await newBook.save();
 
-        res.json({ message: 'Book saved' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+    res.json({message:'Book saved'})
 });
 
-// Ruta para eliminar un libro por su ID (DELETE /api/books/:id)
 router.delete('/:id', async (req, res) => {
-    try {
-        const book = await Book.findByIdAndDelete(req.params.id);
-        unlink(path.resolve('./backend/public' + book.imagePath));
-        res.json({ message: 'Book deleted' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+    const book = await Book.findByIdAndDelete(req.params.id)
+    unlink(path.resolve('./backend/public' + book.imagePath))
+    res.json({message:'Book deleted'})
 });
+
 
 module.exports = router;
